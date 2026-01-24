63a9fa01806e4b302b69ad24f4555576b60fa203
dalrho <shichiya.amistad@gmail.com>
2026-01-25 01:11:48 +0800

    Database URL is not hardcoded

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://health_user:health_password@localhost:5432/healthqr_db"
+SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
